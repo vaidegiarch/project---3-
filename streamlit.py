c3244731a2756500820e111213901ea8cd42b3a8
vaidegiarch <vaidegiarch@gmail.com>
2026-03-21 12:08:43 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,3 @@
-with open("best_random_forest.pkl", "rb") as file:
-    st.download_button(
-        label="Download Model",
-        data=file,
-        file_name="best_random_forest.pkl",
-        mime="application/octet-stream"
-    )
 
 
 
